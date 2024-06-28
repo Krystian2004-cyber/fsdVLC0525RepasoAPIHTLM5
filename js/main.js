8d6a2d93c9f7c0c206d1a91532a2d9d73d608558
Krystian2004-cyber <krystian.kisielow@gmail.com>
2024-06-28 13:14:34 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,9 +2,6 @@ let todosCounter = document.getElementById('todosCounter');
 let todo=document.getElementById('todo');
 let value=0;
 
-let idX=0;
-
-
 function removetodo(id)
 {
     console.log(id);
@@ -21,20 +18,13 @@ function addTodo()
     div.setAttribute("id", id);
     div.addEventListener("click",() => removetodo(id));
     
-    
-
-
     div.innerText = data;
     let list = document.getElementById('todos');
     list.appendChild(div);
 
     todo.value="";
     value++;
-    todosCounter.innerText = "todos count: " + value;
-
-
-
-   
+    todosCounter.innerText = "todos count: " + value;W
 }
 
 
